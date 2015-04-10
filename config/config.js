81dc9c67927626164e979d9252bb0ef5fc427c3f
moJiXiang <mojixiang1102@gmail.com>
2015-04-10 20:35:48 +0800

    修改端口

--- config/config.js
+++ config/config.js
@@ -8,7 +8,7 @@ var config = {
     app: {
       name: 'xinmei-demo'
     },
-    port: 3000,
+    port: 3200,
     db: 'mongodb://localhost/xinmei-demo-development'
   },
 
@@ -17,7 +17,7 @@ var config = {
     app: {
       name: 'xinmei-demo'
     },
-    port: 3000,
+    port: 3200,
     db: 'mongodb://localhost/xinmei-demo-test'
   },
 
@@ -26,7 +26,7 @@ var config = {
     app: {
       name: 'xinmei-demo'
     },
-    port: 3000,
+    port: 3200,
     db: 'mongodb://localhost/xinmei-demo-production'
   }
 };
